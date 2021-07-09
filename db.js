92ad32d946f0589dd3fde192ce56652c6ba92ea3
avreyes <alyssa.va.reyes@gmail.com>
2021-07-09 11:04:40 -0400

    change in dialect

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const {Sequelize} = require('sequelize');
 const sequelize = new Sequelize(
     process.env.DATABASE_URL,
     {
-        dialect: process.env.DATABASE_DIALECT,
+        dialect: 'postgres',
         ssl: process.env.ENVIRONMENT === 'production',
     })
 module.exports = sequelize;
